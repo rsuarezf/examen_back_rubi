69e4d1a62a0d5e5b94e1c091d97ed2668ff9a5df
@suarez.bibi <@suarez.bibi>
2026-03-26 13:03:29 -0600

    cambios finales

---
+++ src/main/resources/db.migration/V1__init.sql
@@ -0,0 +1,20 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Admin
+ * Created: 26 mar. 2026
+ */
+
+
+CREATE DATABASE back
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Mexico.1252'
+    LC_CTYPE = 'Spanish_Mexico.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
